d1374247254dd1a159931c9c99463954a5077b92
JulianVosseberg <17138665+JulianVosseberg@users.noreply.github.com>
2019-12-16 16:44:26 +0100

    Infer LECA introns and get shared LECA introns

--- snellab/map_introns_alignment.py
+++ snellab/map_introns_alignment.py
@@ -9,6 +9,7 @@ import argparse
 import gzip
 from eukarya import supergroups5 as supergroups
 import csv
+import copy
 
 # Functions
 def parse_alignment(fasta_file):
@@ -274,6 +275,81 @@ def cluster_neighbouring_positions(aln_intron_positions, OG_dict, nt_shifts, aa_
                 del introns_shifts[OG][position]
     return introns_shifts
 
+def get_leca_introns(og_introns, supergroups, OG_group_species_count, OG_group_seq_count, threshold_percentage_genes, threshold_percentage_species, threshold_species, outdir):
+    leca_introns = {}
+    leca_count = {}
+    for OG, positions in og_introns.items():
+        leca_count[OG] = 0
+        og_file = open(f'{outdir}/{OG}_introns.tsv', 'w')
+        group_order = [group for group in OG_group_species_count[OG]]
+        og_file.write('Position\tPhase\tLECA\tTotal species (%)\t')
+        og_file.write('\t'.join([group + ' species' for group in group_order]) + '\t')
+        og_file.write('Total sequences (%)\t')
+        og_file.write('\t'.join([group + ' sequences' for group in group_order]) + '\n')
+        spec_total = sum(OG_group_species_count[OG].values())
+        seq_total = sum(OG_group_seq_count[OG].values())
+        leca_introns[OG] = {}
+        for position, phases in positions.items():
+            for phase, seqids in phases.items():
+                seqids = [seqid for seqids in introns_shifts[OG][position][phase] for seqid in seqids]
+                group_species_counts = count_groups(seqids, supergroups, unique = True)
+                opimoda = group_species_counts['Amoebozoa'] + group_species_counts['Obazoa']
+                diphoda = sum(group_species_counts.values()) - opimoda
+                spec_cov = sum(group_species_counts.values()) / spec_total * 100
+                group_seq_counts = count_groups(seqids, supergroups, unique = False)
+                seq_cov = sum(group_seq_counts.values()) / seq_total * 100
+                leca = 'No'
+                if opimoda >= threshold_species and diphoda >= threshold_species:
+                    if spec_cov >= threshold_percentage_species and seq_cov >= threshold_percentage_genes:
+                        leca = 'Yes'
+                        leca_count[OG] += 1
+                        if position in leca_introns[OG]:
+                            leca_introns[OG][position].append(phase)
+                        else:
+                            leca_introns[OG][position] = [phase]
+                print(position, phase, leca, round(spec_cov, 1), sep = '\t', end = '', file = og_file)
+                for group, total in OG_group_species_count[OG].items():
+                    if total != 0:
+                        og_file.write(f'\t{round(group_species_counts[group] / total * 100, 1)}')
+                    else:
+                        og_file.write('\tNA')
+                for group, total in OG_group_seq_count[OG].items():
+                    if total != 0:
+                        og_file.write(f'\t{round(group_seq_counts[group] / total * 100, 1)}')
+                    else:
+                        og_file.write('\tNA')
+                og_file.write('\n')
+        og_file.close()
+    return leca_introns, leca_count
+
+def get_shared_leca_introns(leca_introns, nt_shifts, aa_shifts):
+    OGs = leca_introns.keys()
+    number_of_shared_introns = {}
+    for i in range(len(OGs) - 1):
+        OG1 = tuple(OGs)[i]
+        number_of_shared_introns[OG1] = {}
+        for j in range(i + 1, len(OGs)):
+            OG2 = tuple(OGs)[j]
+            number_of_shared_introns[OG1][OG2] = 0
+            OG2_introns_copy = copy.deepcopy(leca_introns[OG2])
+            for position, phases in leca_introns[OG1].items():
+                for phase1 in phases:
+                    for possible_shift in range(position - aa_shifts, position + aa_shifts + 1):
+                        if possible_shift in OG2_introns_copy:
+                            for phase2 in OG2_introns_copy[possible_shift]:
+                                nt = possible_shift*3 + phase2
+                                if nt in range(position*3 + phase1 - nt_shifts, position*3 + phase1 + nt_shifts + 1):
+                                    number_of_shared_introns[OG1][OG2] += 1
+                                    sys.stderr.write(f'Found a shared intron between {OG1} ({position}.{phase1}) and {OG2} ({possible_shift}.{phase2}).\n')
+                                    OG2_introns_copy[possible_shift].remove(phase2)
+                                    if len(OG2_introns_copy[possible_shift]) == 0:
+                                        del OG2_introns_copy[possible_shift]
+                                    break
+                            else:
+                                continue
+                            break
+    return number_of_shared_introns
+
 # Parse arguments
 parser = argparse.ArgumentParser(description = "This script maps intron positions onto a protein alignment.")
 parser.add_argument("alignment", help = 'protein alignment in fasta format')
@@ -289,6 +365,7 @@ args = parser.parse_args()
 
 # To add: sequence ID --> paralogue table
 
+prefix = args.alignment[:args.alignment.find(".")]
 if args.o:
     output_path = args.o
     if not os.path.isdir(output_path):
@@ -320,8 +397,6 @@ groups = set(supergroups.values())
 # Parse alignment
 with open(args.alignment) as fasta_file:
     species_seqids_dict, OG_dict, aligned_proteins = parse_alignment(fasta_file)
-OG_group_seq_count = {OG : count_groups(seqs, unique = False) for OG, seqs in OG_dict.items()}
-OG_group_species_count = {OG : count_groups(seqs, unique = True) for OG, seqs in OG_dict.items()}
 lengths = {seqid : len(aln.replace('-', '')) for seqid, aln in aligned_proteins.items()}
 
 # Get CDS coordinates
@@ -329,10 +404,18 @@ euk_cds_dict = get_coordinates(species_seqids_dict)
 
 # Step 2: Map intron positions onto the proteins
 location_introns = map_introns_aa(euk_cds_dict, lengths)
+OG_seq_info = {}
+for OG, seqs in OG_dict.items():
+    OG_seq_info[OG] = []
+    for seq in seqs:
+        if seq in location_introns:
+            OG_seq_info[OG].append(seq)
+OG_group_seq_count = {OG : count_groups(seqs, supergroups, unique = False) for OG, seqs in OG_seq_info.items()}
+OG_group_species_count = {OG : count_groups(seqs, supergroups, unique = True) for OG, seqs in OG_seq_info.items()}
 
 # Step 3: Create sequence features file for viewing intron positions in Jalview
 seqid_OG = {seqid : og for og, seqids in OG_dict.items() for seqid in seqids}
-with open(f'{output_path}/{args.alignment[:args.alignment.find(".")]}_jalview.sff', 'w') as features_file:
+with open(f'{output_path}/{prefix}_jalview.sff', 'w') as features_file:
     features_file.write('phase0\tgreen\nphase1\tblue\nphase2\tmagenta\nNA\tblack\n')
     for seqid in aligned_proteins:
         try:
@@ -344,210 +427,48 @@ with open(f'{output_path}/{args.alignment[:args.alignment.find(".")]}_jalview.sf
 
 # Step 4: Map intron positions onto the alignment
 aln_intron_positions = map_introns_aln(location_introns, aligned_proteins, seqid_OG)
+with open(f'{output_path}/{prefix}_introns.tsv', 'w') as all_introns:
+    all_introns.write('Positon\tPhase\tOG\tSequence IDs\n')
+    for OG, positions in aln_intron_positions.items():
+        for position, phases in positions.items():
+            for phase, seqids in enumerate(phases):
+                if len(seqids) == 0:
+                    continue
+                print(position, phase, OG, ','.join(seqids), sep = '\t', file = all_introns)
 
 # Step 5: infer LECA introns
-if not inference:
-    sys.exit()
-
-if shifts <= 3:
-    aa_shifts = 1
-else:
-    if shifts%3 == 0:
-        aa_shifts = shifts/3
+if inference:
+    # Calculate shifts and cluster neighbouring positions
+    if shifts <= 3:
+        aa_shifts = 1
     else:
-        aa_shifts =(shifts - shifts%3)/3 + 1 # 0-3 --> aa=1, 4-6 --> aa=2, 7-9 --> aa=3, ...
-
-introns_shifts = cluster_neighbouring_positions(aln_intron_positions, OG_dict, shifts, aa_shifts)
-for OG in OG_dict:
-    for dominant_position in introns_shifts[OG]:
-        gene_supergroup_dict[OG][dominant_position] = {"Amoebozoa" : [],"Obazoa" : [], "Excavata": [], "Archaeplastida": [], "RASH": []}
-        frames = -1
-        for phase in introns_shifts[OG][dominant_position]:
-            for supergroup in gene_supergroup_dict[OG][dominant_position]:
-                gene_supergroup_dict[OG][dominant_position][supergroup].append([])
-            frames += 1
-            for ID in phase:
-                begin = ID.rfind("_")
-                species = ID[begin+1:begin+5]
-                if species in supergroups:
-                    try:
-                        gene_supergroup_dict[OG][dominant_position][supergroups[species]][frames].append(ID)
-                    except:
-                        sys.stderr.write(f"Warning: something went wrong while composing the gene supergroup dictionary for {ID}.\n")
-
-    # Write mapped introns to output file
-    OG_file = open(f"{output_path}/analysis_{OG}.txt", "w")
-    raw = open(f"{output_path}/raw_data_{OG}.txt", "w")
-
-    #printing a header for all documents:
-    print("Intron position/Supergroup", end = "\t",file = OG_file)
-    print("Intron position/Supergroup", end = "\t",file = raw)
-    for i in range(-shifts, shifts+1, 1):
-        if i == 0:
-            print("dominant phase", end = "\t",file = OG_file)
-            print("dominant phase", end = "\t",file = raw)
+        if shifts%3 == 0:
+            aa_shifts = shifts/3
         else:
-            print(i, end = "\t", file = OG_file)
-            print(i, end = "\t", file = raw)
-    print("Percentage of genes", "\t", "Percentage of species", file = OG_file)
-    print("Percentage of genes", "\t", "Percentage of species", file = raw)
+            aa_shifts =(shifts - shifts%3)/3 + 1 # 0-3 --> aa=1, 4-6 --> aa=2, 7-9 --> aa=3, ...
+    introns_shifts = cluster_neighbouring_positions(aln_intron_positions, OG_dict, shifts, aa_shifts)
+    # Infer intron positions/clusters present in LECA and make an analysis file per OG
+    leca_introns, leca_count = get_leca_introns(introns_shifts, supergroups, OG_group_species_count, OG_group_seq_count, threshold_percentage_genes, threshold_percentage_species, threshold_species, output_path)
+    # Obtain shared LECA introns
+    number_of_shared_introns = get_shared_leca_introns(leca_introns, shifts, aa_shifts)
 
-    for intron_position in sorted(gene_supergroup_dict[OG]):
-        #number_of_species_intron = 0
-        list_with_species_intron =[]
-        genes_per_position = 0
-        percentage_genes_per_group={}
-        percentage_species_per_group = {}
-        opimoda = 0
-        diphoda = 0
-        genes_per_phase={}
-        represented_groups = 0
-        for group in gene_supergroup_dict[OG][intron_position]:
-            phases = 0
-            percentage_genes_per_group[group] = 0
-            percentage_species_per_group[group] = 0
-            genes_per_group = 0
-            species_per_group = 0
-            for phase in gene_supergroup_dict[OG][intron_position][group]:
-                phases+=1
-                if phases not in genes_per_phase:
-                    genes_per_phase[phases] = 0
-
-                #Counting the Opimoda and Diphoda for required threshold.
-                # Counting several variables needed to calulate percentages.
-                for gene in phase:
-                    if group == "Amoebozoa" or group == "Obazoa":
-                        opimoda += 1
-                    else:
-                        diphoda += 1
-                    #begin = gene.rfind("_")+1
-                    #species = gene[begin:begin+4]
-                    species = gene[:4]
-                    if species not in list_with_species_intron:
-                        list_with_species_intron.append(species)
-                        species_per_group +=1
-                        #number_of_species_intron +=1
-                    genes_per_phase[phases] +=1
-                    genes_per_group += 1
-                    genes_per_position += 1
-
-            #Caluclating percentages to see if introns reach the threshold
-            if genes_per_group != 0:
-                percentage_genes_per_group[group] = str((genes_per_group/OG_group_seq_count[OG][group])*100)[0:4]
-                percentage_species_per_group[group] = str((species_per_group/OG_group_species_count[OG][group]*100))[0:4]
-        total_percentage_of_gene = str((genes_per_position/len(OG_dict[OG]))*100)[0:4]
-        percentage_of_species = str((len(list_with_species_intron)/sum(OG_group_species_count[OG].values()))*100)[0:4]
-
-        #Check if introns reach the thresholds to be considered LECA genes
-        if opimoda >= threshold_species and diphoda >= threshold_species:
-            if float(total_percentage_of_gene) > threshold_percentage_genes:
-                if float(percentage_of_species) > threshold_percentage_species:
-
-                    #making the analysis file
-                    k = 0
-                    print(">",intron_position,"\t","Dominant_phase = ", dominant_phases[OG][intron_position], file = OG_file)
-                    if intron_position in LECA_introns:
-                        LECA_introns[intron_position].append(OG)
-                    else:
-                        LECA_introns[intron_position]=[OG]
-                    for group in gene_supergroup_dict[OG][intron_position]:
-                        print(group, end="\t", file = OG_file)
-                        for phase in gene_supergroup_dict[OG][intron_position][group]:
-                            print(','.join(phase),end ="\t",file = OG_file)
-                        print(percentage_genes_per_group[group],"\t",percentage_species_per_group[group],"%",file = OG_file)
-                    for phase in genes_per_phase:
-                        k += 1
-                        percentage_per_phase = str(genes_per_phase[phase]/genes_per_position * 100)[0:4]
-                        if k == 1:
-                            print("total percentage of genes", "\t",percentage_per_phase,"%",end="\t", file = OG_file)
-                        else:
-                            print(percentage_per_phase,"%",end="\t",file = OG_file)
-                    print(total_percentage_of_gene,"%","\t", percentage_of_species,"%","\n", file = OG_file)
-        print(">",intron_position,"\t","Dominant phase = ",dominant_phases[OG][intron_position], file = raw)
-
-        #Builing a file with raw data
-        for group in gene_supergroup_dict[OG][intron_position]:
-            print(group, end="\t", file =raw)
-            for phase in gene_supergroup_dict[OG][intron_position][group]:
-                print(phase,end ="\t",file=raw)
-            print(percentage_genes_per_group[group],file = raw)
-        k = 0
-        for phase in genes_per_phase:
-            if genes_per_position != 0:
-                k += 1
-                percentage_per_phase = str(genes_per_phase[phase]/genes_per_position * 100)[0:4]
-                if k > 1:
-                    print(percentage_per_phase,"%",end="\t",file = raw)
+    # Write output table
+    with open(output_path + '/table.txt', 'w') as table:
+        table.write('\t' + '\t'.join(OG_dict.keys()) + '\n')
+        OGs = list(OG_dict.keys())
+        for i, OG1 in enumerate(OGs):
+            table.write(OG1)
+            for j in range(i + 1):
+                OG2 = OGs[j]
+                if OG1 == OG2:
+                    table.write(f'\t{leca_count[OG1]}')
                 else:
-                    print("total percentage of genes", "\t",percentage_per_phase,"%",end="\t", file =raw)
-        print(total_percentage_of_gene,"%","\n", file = raw)
-    raw.close()
-    OG_file.close()
-
-# Make a log file
-logfile = open(output_path+"/logfile.txt","w")
-print("threshold percentage genes = ",threshold_percentage_genes,"threshold percentage species: ",threshold_percentage_species,"\t","Shifts = ", shifts, file=logfile)
-for intron_position in LECA_introns:
-    print("\nWe found a LECA intron on position ",intron_position," of the alignment", file= logfile)
-    for OG in LECA_introns[intron_position]:
-        print("This LECA intron is found in ", OG,"in Phase ",dominant_phases[OG][intron_position], file = logfile)
-print("\nScore:", file = logfile)
-number_of_shared_introns = {}
-for OG in OG_dict:
-    score = {}
-    number_of_shared_introns[OG]={}
-    for LECA_intron in LECA_introns:
-        if OG in LECA_introns[LECA_intron]:
-            #Because shifts should be taken in account
-            for possible_shift in range(LECA_intron-aa,LECA_intron+aa+1,1):
-                if possible_shift != LECA_intron:
-                    if possible_shift in LECA_introns:
-                        for other_OG in LECA_introns[possible_shift]:
-                            k=0
-                            for hypothetical_phase in range(LECA_intron*3+dominant_phases[OG][LECA_intron]-shifts,LECA_intron*3+dominant_phases[OG][LECA_intron]+shifts+1,1):
-                                if k == 0:
-                                    if hypothetical_phase in range(possible_shift*3+dominant_phases[other_OG][possible_shift]-shifts,possible_shift*3+dominant_phases[other_OG][possible_shift]+shifts+1,1):
-                                        k+=1
-                                        if other_OG in number_of_shared_introns[OG]:
-                                            number_of_shared_introns[OG][other_OG]+=1
-                                            print(OG,other_OG)
-                                        else:
-                                            number_of_shared_introns[OG][other_OG] = 1
-                else:#I deliberately used the if else statements below (the same as above), because I thought using this else would save the computer a lot of calculating
-                    for other_OG in LECA_introns[LECA_intron]:
-                        if other_OG in number_of_shared_introns[OG]:
-                            number_of_shared_introns[OG][other_OG] += 1
-                        else:
-                            number_of_shared_introns[OG][other_OG] = 1
-logfile.close()
-table = open(output_path+"/table.txt","w")
-new_list = []
-lines = {}
-first_line = []
-for OG in number_of_shared_introns:
-    first_line.append(OG)
-    lines[OG] = [OG]
-    new_list.append(OG)
-    for other_OG in new_list:
-        if other_OG == OG:
-            lines[OG].append("1.0")
-        elif other_OG in number_of_shared_introns[OG]:
-            #Calculating the score
-            score = str(number_of_shared_introns[OG][other_OG]/(number_of_shared_introns[other_OG][other_OG]))[0:4]
-            lines[OG].append(float(score))
-        else:
-            lines[OG].append(0.0)
-k = 0
-for OG in first_line:
-    k+=1
-    if k < len(first_line):
-        print("\t", end= OG ,file=table)
-    else:
-        print("\t", OG, file = table)
-for OG in lines:
-    for value in lines[OG]:
-        if value == "1.0":
-            print(value,file = table)
-        else:
-            print(value,end ="\t", file = table)
-table.close()
+                    if leca_count[OG1] == 0:
+                        table.write('\t0')
+                        continue
+                    try:
+                        number_shared = number_of_shared_introns[OG1][OG2]
+                    except KeyError:
+                        number_shared = number_of_shared_introns[OG2].get(OG1, 0)
+                    table.write(f'\t{number_shared}')
+            table.write('\n')
