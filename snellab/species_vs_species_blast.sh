f6f67c976d53c4de4a470baad230709eb792a511
JulianVosseberg <17138665+JulianVosseberg@users.noreply.github.com>
2021-12-09 13:23:54 +0100

    Replace sequence selection script for blast db

--- snellab/species_vs_species_blast.sh
+++ snellab/species_vs_species_blast.sh
@@ -5,7 +5,7 @@
 function usage() {
     echo -e "Usage: \n\tspecies_vs_species_blast.sh <file.fa> <cpus> <outputdir>\n"
     echo "Fasta file should only contain sequences starting with a four-letter abbreviation"
-    echo "Dependencies: selectSequences.pl, makeblastdb and blastp"
+    echo "Dependencies: select_sequences.py, makeblastdb and blastp"
     exit
 }
 
@@ -28,12 +28,13 @@ list=$(cat $outdir/${prefix}_ids)
 # Create file with sequences per species and make a Blast database
 for species in $list; do
     speciesfa=$outdir/${prefix}_$species.fa
-    selectSequences.pl -i $fasta -q $species > $speciesfa
+    select_sequences.py -q $species $fasta > $speciesfa
     makeblastdb -in $speciesfa -dbtype prot
 done
 
 # Run all-versus-all-Blastp, excluding your own species. Only one target sequence is reported
 for species1 in $list; do
+    echo "BLASTing sequences of $species1..."
     for species2 in $list; do
 	if [ $species1 != $species2 ]; then
 	    blastp -query $outdir/${prefix}_$species1.fa -db $outdir/${prefix}_$species2.fa -max_target_seqs 1 -outfmt 6 -num_threads $cpus >> $outdir/${prefix}_blastp.txt
